40976d48bd7773fa05db037bd8cea008d2e226cb
Blake-Whitham <bwhitham77@gmail.com>
2020-12-01 14:58:56 -0800

    Finished and Tested CRUD operations

--- controllers/listing.js
+++ controllers/listing.js
@@ -11,25 +11,60 @@ getListings = (req, res) => {
   const query = listings.ListingsModel.where({ listing_id: req.params.id });
   mongo.connect();
   query.find((err, listings) => {
-   if (err) {
-     res.status(404)
-     console.log(err.message);
-     mongo.db.close();
-   } else {
+    if (err) {
+      res.status(404)
+      console.log(err.message);
+      mongo.db.close();
+    } else {
       res.status(200).send(listings);
       mongo.db.close();
     }
   }
- )
+  )
 }
+
 post = (req, res) => {
-
+  mongo.connect();
+  listings.write(req.body, (err, data) => {
+    if (err) {
+      res.status(404).send('error');
+      console.log(err.message);
+      mongo.db.close();
+    } else {
+      res.status(200).send(data);
+      mongo.db.close();
+    }
+  })
 }
+
 put = (req, res) => {
-
+  mongo.connect();
+  const query = listings.ListingsModel.where({ listing_id: req.params.id });
+  listings.updateListing(query, req.body, (err) => {
+    if (err) {
+      res.status(404).send('error');
+      console.log(err.message);
+      mongo.db.close();
+    } else {
+      res.status(200).send('success');
+      mongo.db.close();
+    }
+  })
 }
-remove = (req, res) => {
 
+remove = (req, res) => {
+  mongo.connect();
+  const query = listings.ListingsModel.where({ listing_id: req.params.id });
+  listings.removeListing(query, (err) => {
+    if (err) {
+      res.status(404).send('error');
+      console.log(err.message);
+      mongo.db.close();
+    } else {
+      res.status(200).send('success');
+      mongo.db.close();
+    }
+  })
 }
 
 module.exports = {
