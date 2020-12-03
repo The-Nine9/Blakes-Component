532a2eacf23fb0f72b71ea9494be54ccbf2b61b7
Blake-Whitham <bwhitham77@gmail.com>
2020-12-03 11:56:54 -0800

    added notes

--- SCHEAMA.sql
+++ SCHEAMA.sql
@@ -10,7 +10,7 @@ WITH
 
 
 DROP TABLE IF EXISTS listing;
-DROP TABLE IF EXISTS header;
+-- DROP TABLE IF EXISTS header;–
 DROP TABLE IF EXISTS images;
 
 CREATE TABLE [IF NOT EXISTS] listing (
@@ -20,25 +20,21 @@ CREATE TABLE [IF NOT EXISTS] listing (
   price INTEGER NOT NULL CHECK ( price > 0 AND price < 1000000000),
   bed SMALLINT NOT NULL CHECK ( bed > 0 AND bed < 100 ),
   bath SMALLINT NOT NULL CHECK ( bath > 0 AND bath < 100 ),
-  PRIMARY KEY(header_no),
-    CONSTRAINT fk_header
-      FOREIGN KEY(header_no)
-	      REFERENCES header(header_no)
-);
-
-CREATE TABLE [IF NOT EXISTS] header (
-  header_no SERIAL NOT NULL PRIMARY KEY,
-  sale BOOLEAN NOT NULL,
+  sale BOOLEAN NOT NULL, -- lose "not null" -> default false
   pending BOOLEAN NOT NULL,
   new BOOLEAN NOT NULL,
   construction BOOLEAN NOT NULL,
 );
 
-CREATE TABLE [IF NOT EXISTS] images (
+-- CREATE TABLE [IF NOT EXISTS] header (
+--   header_no SERIAL NOT NULL PRIMARY KEY,
+-- );
+
+CREATE TABLE [IF NOT EXISTS] images ( -- keep track of image order
   image_no SERIAL NOT NULL PRIMARY KEY,
   listing INT NOT NULL FOREIGN KEY,
   images VARCHAR(100) NOT NULL,
-    CONSTRAINT fk_listing
+    CONSTRAINT fk_listing -- lose foreign for seeding
       FOREIGN KEY(listing_no)
 	      REFERENCES listing(listing_no)
 );
