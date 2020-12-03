7e10427e16c5cb10014e43683fe435ebc5408e81
Blake-Whitham <bwhitham77@gmail.com>
2020-12-02 22:55:27 -0800

    Added steps to create collection

--- arangodb.js
+++ arangodb.js
@@ -1,28 +1,88 @@
-const address =
-{
-  rule: {
-    address: {
-      type: "object",
-      properties: {
-        number: { type: "number" },
-        street_name: { type: "string" },
-      },
-    },
-  },
-};
+const db = require('arangojs')();
 
+
+db.createDatabase('mydb')
+  .then(
+    () => console.log('Database created'),
+    (err) => console.error('Failed to create database:', err),
+  );
+db.useDatabase('mydb'); // add to above promise?
 
 const schema = {
-  listing: { unique: true, type: Number },
-  header: {
-    sale: Boolean,
-    pending: Boolean,
-    new: Boolean,
-    construction: Boolean,
+  message: 'Customer Validation Failed',
+  level: 'strict',
+  rule: {
+    type: 'object',
+    properties: {
+      listing_id: {
+        type: 'number',
+        min: 0,
+      },
+      address: {
+        type: 'string',
+        minLength: 2,
+        maxLength: 100,
+        pattern: /^\d+\s[A-z]+\s[A-z]+/g,
+      },
+      price: {
+        type: 'number',
+        minimum: 0,
+        maximum: 1000000000,
+      },
+      bed: {
+        type: 'number',
+        minimum: 0,
+        maximum: 20,
+      },
+      bath: {
+        type: 'number',
+        minimum: 0,
+        maximum: 20,
+      },
+      images: {
+        type: 'array',
+        miniItems: 5,
+        maxItems: 80,
+      },
+      sale: { // would like to refactor to be status (enum [ sale, pending, new, construction ])
+        type: 'boolean',
+      },
+      pending: {
+        type: 'boolean',
+      },
+      new: {
+        type: 'boolean',
+      },
+      construction: {
+        type: 'boolean',
+      },
+    },
+    required: [
+      'listing_id',
+      'sale',
+      'address',
+      'price',
+      'bed',
+      'bath',
+      'images',
+    ],
+    additionalProperties: 'False',
   },
-  address: { unique: true, type: String },
-  price: Number,
-  bed: Number,
-  bath: Number,
-  images: Array,
+};
+db.drop('listings');
+db.create('listings', { schema })
+  .then(
+    () => console.log('Collection created'),
+    (err) => console.error('Failed to create collection:', err),
+  );
+
+/* Bulk Seeding example
+let docs = [];
+for (i = 0; i < 100; i++) {
+  docs.push({_key: `doc${i + 1}`, value: i});
 }
+collection.import(docs).then(
+  result => console.log('Import complete:', result),
+  err => console.error('Import failed:', err)
+);
+*/
