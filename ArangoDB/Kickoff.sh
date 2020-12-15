f8602af55cddb20bd95f8b1d95dfc073f16ab921
Blake-Whitham <bwhitham77@gmail.com>
2020-12-15 08:54:02 -0800

    rearanged kickoff

--- ArangoDB/Kickoff.sh
+++ ArangoDB/Kickoff.sh
@@ -7,8 +7,8 @@
 ## node -e "console.log('Running Node.js ' + process.version)"
 # My first script
   chmod 777 ArangoDB/setAuth.sh;
+  sudo yum -y install cowsay
   cd /etc/yum.repos.d/;
   sudo curl -OL https://download.arangodb.com/arangodb37/RPM/arangodb.repo;
   sudo yum -y install arangodb3-3.7.5-1.0;
   sudo arango-secure-installation;
-  sudo yum -y install cowsay
